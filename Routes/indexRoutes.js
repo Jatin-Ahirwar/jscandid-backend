1658e619bcb302ee0150a6e610db9f6fffe5086d
Jatin-Ahirwar <jatinahirwar24@gmail.com>
2024-01-01 23:37:39 +0530

    signout route changes

--- Routes/indexRoutes.js
+++ Routes/indexRoutes.js
@@ -17,7 +17,7 @@ router.post("/signup",adminsignup)
 
 router.post("/signin",adminsignin)
 
-router.get("/signout",adminsignout)
+router.get("/signout", isAuthenticated ,adminsignout)
 
 
 module.exports = router
