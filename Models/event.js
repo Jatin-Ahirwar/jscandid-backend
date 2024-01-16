0f7c1eef24ca252f226ee1627e4f2cca178920a8
Jatin-Ahirwar <jatinahirwar24@gmail.com>
2024-01-17 01:31:15 +0530

    data show done

--- Models/event.js
+++ Models/event.js
@@ -17,6 +17,19 @@ const eventModel = new mongoose.Schema({
         maxlength:[15,"model name can not exceed 15 characters"],
         minlength:[2,"model name should contain minimum 2 characters"]
     },
+    country:{
+        type:String,
+        required:[true,"country is required"],
+        maxlength:[20,"country should not exceed more than 15 character"],
+        minlength:[3,"country should have atleast 3 characters"]
+    },
+    location:{
+        type:String,
+        required:[true,"location is required"],
+        maxlength:[12,"location should not exceed more than 15 character"],
+        minlength:[2,"location should have atleast 6 characters"]
+    },
+
 })
 
 const event = mongoose.model("event",eventModel)
