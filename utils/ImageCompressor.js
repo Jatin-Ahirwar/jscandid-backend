3bcd5bc0b17701b31bd2b15cc6f5a98c9615568b
Jatin-Ahirwar <jatinahirwar24@gmail.com>
2024-02-13 22:23:14 +0530

    automatic rotation error resolved

--- utils/ImageCompressor.js
+++ utils/ImageCompressor.js
@@ -1,3 +1,23 @@
+// // imageCompression.js
+// const sharp = require('sharp');
+
+// exports.ImageCompressor = async (inputFileBuffer, quality = 80) => {
+//     try {
+//         // Compress the image buffer
+//         const compressedBuffer = await sharp(inputFileBuffer)
+//             .jpeg({ quality: quality }) // You can change to 'webp' or other formats if needed
+//             .toBuffer();
+
+//         console.log('Compression successful.');
+
+//         return compressedBuffer;
+//     } catch (error) {
+//         console.error('Error compressing file:', error.message);
+//         throw error; // Re-throw the error to handle it in the calling code
+//     }
+// }
+
+
 // imageCompression.js
 const sharp = require('sharp');
 
@@ -6,6 +26,7 @@ exports.ImageCompressor = async (inputFileBuffer, quality = 80) => {
         // Compress the image buffer
         const compressedBuffer = await sharp(inputFileBuffer)
             .jpeg({ quality: quality }) // You can change to 'webp' or other formats if needed
+            .withMetadata()
             .toBuffer();
 
         console.log('Compression successful.');
@@ -16,34 +37,3 @@ exports.ImageCompressor = async (inputFileBuffer, quality = 80) => {
         throw error; // Re-throw the error to handle it in the calling code
     }
 }
-
-
-
-// // imageCompression.js
-// const sharp = require('sharp');
-// const fs = require('fs');
-// const path = require('path');
-
-// exports.ImageCompressor = async (inputFileBuffer, outputFilePath, quality = 80) => {
-//     try {
-//         const outputDirectory = path.dirname(outputFilePath);
-
-//         // Create the directory if it doesn't exist
-//         if (!fs.existsSync(outputDirectory)) {
-//             fs.mkdirSync(outputDirectory, { recursive: true });
-//         }
-
-//         // Compress the image buffer
-//         const compressedBuffer = await sharp(inputFileBuffer)
-//             .jpeg({ quality: quality }) // You can change to 'webp' or other formats if needed
-//             .toBuffer();
-
-//         // Write the compressed buffer to the output file
-//         fs.writeFileSync(outputFilePath, compressedBuffer);
-
-//         console.log(`Compression successful. Output file saved at: ${outputFilePath}`);
-//     } catch (error) {
-//         console.error('Error compressing file:', error.message);
-//         throw error; // Re-throw the error to handle it in the calling code
-//     }
-// }
